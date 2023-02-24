bf1cca39876ce2f7d4448c273820fee8ec88fe6a
whitehole07 <danieleagamennone@gmail.com>
2023-02-24 12:35:52 +0100

    nn completed, debugging

--- deep-learning/activation_functions.py
+++ deep-learning/activation_functions.py
@@ -13,3 +13,16 @@ def relu(z):
     """
     A = np.maximum(0, z)
     return A
+
+
+def drelu(z):
+    """
+    Computes the derivative of the Rectified Linear Unit (ReLU) activation function for a given input.
+
+    Arguments:
+    z -- numpy array of any shape
+
+    Returns:
+    A -- output of derivative of ReLU(z), same shape as z
+    """
+    return np.greater(z, 0).astype(int)
--- deep-learning/nn.py
+++ deep-learning/nn.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from activation_functions import relu
+from activation_functions import relu, drelu
 
 
 class Neuron(object):
@@ -34,9 +34,10 @@ class Layer(object):
     def out(self, z: np.ndarray):
         # Compute layer output given input z
         if not self.__input_layer:
-            return relu(np.dot(self.W, z) + self.B)
+            z = np.dot(self.W, z) + self.B
+            return z, relu(z)
         else:
-            return z
+            return z, z
 
 
 class FFNN(object):
@@ -79,7 +80,7 @@ class FFNN(object):
         # Init losses
         self.losses = []
 
-        # Mini-batches Stochastic Gradient Descent (SGD)
+        # Mini-batches Stochastic Gradient Descent (mSGD)
         # Train for the specified number of epochs
         for epoch in range(epochs):
             # Shuffle the training data
@@ -95,43 +96,145 @@ class FFNN(object):
             for X_batch, y_batch in zip(X_batches, y_batches):
                 # Forward propagation
                 y_pred_batch = np.array([])
-                activations_batch = np.array([])
+                activations_batch = []
+                zs_batch = []
                 for X_row, y_row in zip(X_batch, y_batch):  # Iterate over batch samples
-                    activations = self.__feed_forward(X_row)  # Propagate single sample
+                    zs, activations = self.__feed_forward(X_row)  # Propagate single sample
 
                     # Update batch prediction
-                    activations_batch = np.append(activations_batch, activations)
-                    y_pred_batch = np.append(y_pred_batch, activations[-1])
+                    activations_batch.append(activations)
+                    zs_batch.append(zs)
+                    y_pred_batch = np.vstack((y_pred_batch, activations[-1]))
 
                 # Backpropagation
-                for activations_batch_row, y_pred_batch_row, y_batch_row in zip(activations_batch, y_pred_batch, y_batch):
-                    self.__back_propagation(y_pred_batch_row, y_batch_row, activations_batch_row)
+                self.__back_propagation(y_pred_batch, y_batch, zs_batch, activations_batch)
 
-            # Evaluate MSE at end of epoch
-            # self.MSE = np.mean((1 / self.n_outputs) * np.sum((y_pred - y_batch) ** 2, axis=1))
-            # self.losses.append(self.MSE)
+                # Evaluate MSE at end of epoch
+                y_pred = self.predict(X_batch)
+                self.MSE = np.mean((1 / self.n_outputs) * np.sum((y_pred - y_batch) ** 2, axis=1))
+                self.losses.append(self.MSE)
+
+    def predict(self, z_in: np.ndarray):
+        # Feed Forward
+        _, activations = self.__feed_forward(z_in)
+        return activations[-1]  # Network Activations
 
     def __feed_forward(self, z_in: np.ndarray):
         if len(self.layers) < 2:
             raise ValueError("Output layer was not set, add hidden layer")
 
         # Feed Forward
-        z = z_in  # Init state
+        a = z_in  # Init state
         activations = []
+        zs = []
         for layer in self.layers:
-            z = layer.out(z)       # Propagate state
-            activations.append(z)  # Update activations
-        return activations  # Network Activations
+            z, a = layer.out(a)    # Propagate state
+            zs.append(z)           # Update states
+            activations.append(a)  # Update activations
+        return zs, activations  # Network Activations
 
-    def __back_propagation(self, y_pred, y_batch, activations):
+    def __back_propagation(self, y_pred_batch, y_batch, zs_batch, activations_batch):
+        # Init gradient matrices
+        dCw = []
+        dCb = []
+
+        # Iterate over batch samples
+        for zs_batch_row, activations_batch_row, y_pred_batch_row, y_batch_row in zip(zs_batch, activations_batch, y_pred_batch, y_batch):
+            # Init batch matrices
+            dCwl = [[] for _ in range(len(self.layers)-1)]
+            dCbl = [[[] for _ in range(self.layers[x])] for x in range(len(self.layers)-1, 0, -1)]
+
+            # Last layer
+            delta_l_next = []
+            for j in range(self.layers[-1].layer_size):
+                delta_l_next.append((2 / self.n_outputs) * (y_batch_row[j] - y_pred_batch_row[j]) * drelu(zs_batch_row[-1][j]))
+
+                # Compute weight gradients
+                for z in range(self.layers[-2].layer_size):
+                    dCwl[-1][j].append(delta_l_next[-1] * activations_batch_row[-2][z])  # here j is in layer l and z in layer l-1
+
+            # Add gradients
+            dCbl[-1].append(delta_l_next)
+
+            # Iterate over next layers
+            for l in range(len(self.layers)-2, 0, -1):  # Exclude output and input layers
+                # Next layer
+                delta_l = []
+                for j in range(self.layers[l].layer_size):
+                    dl = 0
+                    for k in range(self.layers[l+1].layer_size):
+                        dl += delta_l_next[k] * self.layers[l+1].W[j, k] * drelu(zs_batch_row[l][j])
+                    delta_l.append(dl)
+
+                    # Compute weight gradients
+                    for z in range(self.layers[l-1].layer_size):
+                        dCwl[l][j].append(delta_l[-1] * activations_batch_row[l-1][z])  # here j is in layer l and z in layer l-1
+
+                # Add gradients
+                dCbl.append(delta_l)
+
+                # Clear delta_l_next
+                delta_l_next = delta_l
+
+            # Batch completed, update matrix
+            dCw.append(dCwl[::-1])
+            dCb.append(dCbl[::-1])
+
+        # Average out over the batches
+        dCw_avg = self.__avg_gradient_weight(dCw)
+        dCb_avg = self.__avg_gradient_bias(dCb)
+
+        # Update parameters
+        for l in range(len(self.layers)-1, 0, -1):
+            for k in range(self.layers[l].layer_size):
+                # Bias
+                self.layers[l].B[k] -= self.alpha * dCb_avg[l][k]          # Update layer
+                self.layers[l].neurons[k].b -= self.alpha * dCb_avg[l][k]  # Update neuron
+
+                # Weight
+                for j in range(self.layers[l-1].layer_size):
+                    self.layers[l].W[k, j] -= self.alpha * dCw_avg[l][k][j]          # Update layer
+                    self.layers[l].neurons[k].w[j] -= self.alpha * dCw_avg[l][k][j]  # Update neuron
 
 
+    @staticmethod
+    def __avg_gradient_bias(dCb):
+        n = len(dCb)
+        m = len(dCb[0])
+        k = len(dCb[0][0])
 
-        delta = y_pred - y_batch
-        for i in range(len(weights) - 1, -1, -1):
-            dz = np.where(activations[i + 1] > 0, delta, 0)
-            dw = np.dot(activations[i].T, dz) / self.batch_size
-            db = np.mean(dz, axis=0, keepdims=True)
-            delta = np.dot(dz, weights[i].T)
-            weights[i] -= self.alpha * dw
-            biases[i] -= self.alpha * db
+        avg = [[0] * k for _ in range(m)]
+
+        for i in range(n):
+            for j in range(m):
+                for l in range(k):
+                    avg[j][l] += dCb[i][j][l]
+
+        for j in range(m):
+            for l in range(k):
+                avg[j][l] /= n
+
+        return avg
+
+    @staticmethod
+    def __avg_gradient_weight(dCw):
+        n = len(dCw)
+        p = len(dCw[0])
+        m = len(dCw[0][0])
+        k = len(dCw[0][0][0])
+
+        avg = [[[0] * k for _ in range(m)] for _ in range(p)]
+
+        for i in range(n):
+            for j in range(p):
+                for l in range(m):
+                    for o in range(k):
+                        avg[j][l][o] += dCw[i][j][l][o]
+
+        for j in range(p):
+            for l in range(m):
+                for o in range(k):
+                    avg[j][l][o] /= n
+
+        return avg
+
